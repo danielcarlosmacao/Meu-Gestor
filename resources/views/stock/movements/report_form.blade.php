33f5f4bb2e7f838963276368e6f24964316ea754
danielcarlosmacao <danielcarlosmacao1@gmail.com>
2025-10-22 09:45:57 -0400

    relatorio de movimentao de estoque versao 1

--- resources/views/stock/movements/report_form.blade.php
+++ resources/views/stock/movements/report_form.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <h2>Relatório de Movimentações de Estoque</h2>
-    <form action="{{ route('stock.movements.reportView') }}" method="GET" class="row g-3">
+    <form action="{{ route('stock.movements.reportView') }}" method="GET" class="row g-3"  target="_blank">
         <div class="col-md-4">
             <label for="start_date" class="form-label">Data Inicial</label>
             <input type="date" name="start_date" class="form-control" required>
@@ -24,7 +24,7 @@
         </div>
 
         <div class="col-12">
-            <button class="btn btn-primary mt-3" type="submit">
+            <button class="btn btn-primary mt-3" type="submit" >
                 <i class="bi bi-search"></i> Gerar Relatório
             </button>
         </div>
