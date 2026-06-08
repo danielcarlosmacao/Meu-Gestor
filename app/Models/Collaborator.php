6bacaa94f0d51d61a8257419b0cc996a49913af3
danielcarlosmacao <danielcarlosmacao1@gmail.com>
2026-06-08 13:51:37 -0400

    correação letar minuscula

--- app/Models/Collaborator.php
+++ app/Models/Collaborator.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 use Illuminate\Database\Eloquent\Model;
 
-class collaborator extends Model
+class Collaborator extends Model
 {
     use SoftDeletes;
 
